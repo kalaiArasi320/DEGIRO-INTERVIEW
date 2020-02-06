8c8a367f9207449ef55418e9d98bea5eeb131146
kalai <kalaiarasishiva@gmail.com>
2020-02-06 10:55:17 +0100

    Fourth Commit

--- ff/src/test/java/runner/TestRunner.java
+++ ff/src/test/java/runner/TestRunner.java
@@ -7,7 +7,10 @@ import cucumber.api.CucumberOptions;
 @RunWith(Cucumber.class)
 @CucumberOptions(
 		features = "src/test/resources/functionalscenario/form.feature",
-		glue= {"stepDefinitions"}
+		glue= {"src/test/java/stepDefinitions/steps.java"},
+	    //dryRun=true, (used to check whether all feature file statement have corresponding step definition)
+		monochrome=true, //(console output of the cucumber will be in most readable form)
+		format= {"pretty", "html:target"}
 		)
 public class TestRunner {
 
